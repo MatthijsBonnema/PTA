8c6efa1c14fc945f5b7ae3906f277d883a248b5e
simondewit <simon@witoos.nl>
2015-05-07 11:41:12 +0200

    1.2 worked on

--- PTA_1.2.py
+++ PTA_1.2.py
@@ -8,6 +8,7 @@ import sys
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import wordnet
 import nltk
+from collections import defaultdict
 
 def main():
     file = open("ada_lovelace.txt", 'r')
@@ -19,6 +20,33 @@ def main():
     relative = wordnet.synsets("relative", pos='n')
     science = wordnet.synsets("science", pos='n')
     illness = wordnet.synsets("illness", pos='n')
+
+    entities = {
+        "act": wordnet.synsets("act", pos='n'),
+        "animal": wordnet.synsets("animal", pos='n'),
+        "artifact": wordnet.synsets("artifact", pos='n'),
+        "attribute": wordnet.synsets("attribute", pos='n'),
+        "body": wordnet.synsets("body", pos='n'),
+        "cognition": wordnet.synsets("cognition", pos='n'),
+        "communication": wordnet.synsets("communication", pos='n'),
+        "event": wordnet.synsets("event", pos='n'),
+        "feeling": wordnet.synsets("feeling", pos='n'),
+        "food": wordnet.synsets("food", pos='n'),
+        "group": wordnet.synsets("group", pos='n'),
+        "location": wordnet.synsets("location", pos='n'),
+        "motive": wordnet.synsets("motive", pos='n'),
+        #"natural object": wordnet.synsets("natural object", pos='n'),
+        "person": wordnet.synsets("person", pos='n'),
+        "plant": wordnet.synsets("plant", pos='n'),
+        "possession": wordnet.synsets("possession", pos='n'),
+        "process": wordnet.synsets("process", pos='n'),
+        "quantity": wordnet.synsets("quantity", pos='n'),
+        "relation": wordnet.synsets("relation", pos='n'),
+        "shape": wordnet.synsets("shape", pos='n'),
+        "state": wordnet.synsets("state", pos='n'),
+        "substance": wordnet.synsets("substance", pos='n'),
+        "time": wordnet.synsets("time", pos='n')
+    }
 
     noun_lemmas = []
     noun_synsets_relative = []
@@ -48,11 +76,17 @@ def main():
     print("Science: ", len(noun_synsets_science))
 
     # Exercise 1.2
-    print("\nEXERCISE 1.3\n")
+    print("\nEXERCISE 1.2\n")
+    tagged_top_entities = defaultdict(list)
     for word in pos_tags:
         if word[1] == "NN" or word[1] == "NNP":
             noun_lemmas.append(lemmatizer.lemmatize(word[0], wordnet.NOUN))
-            for
+            word_synset = wordnet.synsets(word[0], pos="n")
+            for e in list(entities.keys()):
+                if len(word_synset) != 0 and len(entities[e]):
+                    if hypernymOf(word_synset[0], entities[e][0]):
+                        tagged_top_entities[word[0]].extend(e)
+    print(tagged_top_entities)
 
     # Exercise 1.3
     print("\nEXERCISE 1.3\n")
