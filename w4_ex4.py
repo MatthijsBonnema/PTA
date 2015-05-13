7028b87260d4d605992cc64c6d759b0daeac5b10
simondewit <simon@witoos.nl>
2015-05-13 12:28:40 +0200

    worked on ex1 w4

--- w4_ex4.py
+++ w4_ex4.py
@@ -5,6 +5,7 @@
 # Info:
 
 import os
+import nltk
 
 path = "group9/"
 dirs = ["p34", "p35"]
@@ -15,5 +16,9 @@ for directory in dirs:
                 with open(os.path.join(path, directory+"/"+directory2, filename), 'r') as filedata:
                     text_data = [line.split() for line in filedata]
 tokens = [token_data[3] for token_data in text_data]
-print(tokens)
+tagged_tokens = nltk.pos_tag(tokens)
+for i in range(len(text_data)):
+    text_data[i].append(tagged_tokens[i][1])
+for i in text_data:
+    print(i)
 
