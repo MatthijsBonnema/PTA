0e567163abbb8a31be942ed5c4a8b3990412abb0
MatthijsBonnema <m.s.bonnema@student.rug.nl>
2015-05-20 21:00:20 +0200

    Push dat shit

--- disambiguiate.py
+++ disambiguiate.py
@@ -17,13 +17,17 @@ def main(argv):
                     ambigious_words.append((l[2], l[3]))
     with open(argv[1], 'r') as fname:
         for word in ambigious_words:
+            lines = []
             start = int(str(word[0][0]) + "001")
             end = start + 999
             for line in fname:
                 l = line.split()
-                if start <= l[2] <= end:
-                    ambigious_lines.append(line)
-    print(ambigious_words, ambigious_lines)
+                if start <= int(l[2]) <= end:
+                    lines.append(l[3])
+            ambigious_lines.append(lines)
+    # print(ambigious_words, ambigious_lines)
+    for i in range(len(ambigious_lines)):
+        print(ambigious_words[i], ambigious_lines[i])
 
 
 
