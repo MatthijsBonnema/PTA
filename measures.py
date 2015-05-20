77f56da8c9d916704f817d6929d757d7570c85a1
simondewit <simon@witoos.nl>
2015-05-20 12:44:03 +0200

    edited for our comparison

--- measures.py
+++ measures.py
@@ -10,44 +10,46 @@ def makeList(f):
                 tagged.append(line.split()[5])
     return tagged
 
-compare = ["en.tok.off.pos.simon", "en.tok.off.pos.lars", "en.tok.off.pos.matthijs"]
+compare = ["en.tok.off.pos.simon2", "en.tok.off.pos.lars2", "en.tok.off.pos.matthijs2"]
 simon = makeList(compare[0])
 lars = makeList(compare[1])
 matthijs = makeList(compare[2])
 
-cm = ConfusionMatrix(simon, lars)
-#cm2 = ConfusionMatrix(simon, matthijs)
-#cm3 = ConfusionMatrix(lars, matthijs)
-
-print(cm)
+cm_list = [ConfusionMatrix(simon, lars), ConfusionMatrix(simon, matthijs), ConfusionMatrix(lars, matthijs)]
 
 # deze moeten cou, cit, etc worden
 labels = set('COU CIT ENT PER ANI NAT ORG'.split())
 
-true_positives = Counter()
-false_negatives = Counter()
-false_positives = Counter()
+n = 0
+for cm in cm_list:
+    print(compare[n] + "\n")
+    n += 1
+    print(cm)
+    true_positives = Counter()
+    false_negatives = Counter()
+    false_positives = Counter()
 
-# hier wordt geteld wat de aantal keren hits van de classes in de confusion matrix zijn
-for i in labels:
-    for j in labels:
-        if i == j:
-            print(i, j)
-            true_positives[i] += cm[i, j]
+    # hier wordt geteld wat de aantal keren hits van de classes in de confusion matrix zijn
+    for i in labels:
+        for j in labels:
+            if i == j:
+                print(i, j)
+                true_positives[i] += cm[i, j]
+            else:
+                false_negatives[i] += cm[i, j]
+                false_positives[j] += cm[i, j]
+
+    print("TP:", sum(true_positives.values()), true_positives)
+    print("FN:", sum(false_negatives.values()), false_negatives)
+    print("FP:", sum(false_positives.values()), false_positives)
+    print("\n")
+
+    for i in sorted(labels):
+        if true_positives[i] == 0:
+            fscore = 0
         else:
-            false_negatives[i] += cm[i, j]
-            false_positives[j] += cm[i, j]
-
-print("TP:", sum(true_positives.values()), true_positives)
-print("FN:", sum(false_negatives.values()), false_negatives)
-print("FP:", sum(false_positives.values()), false_positives)
-print() 
-
-for i in sorted(labels):
-    if true_positives[i] == 0:
-        fscore = 0
-    else:
-        precision = true_positives[i] / float(true_positives[i]+false_positives[i])
-        recall = true_positives[i] / float(true_positives[i]+false_negatives[i])
-        fscore = 2 * (precision * recall) / float(precision + recall)
-    print(i, fscore)
+            precision = true_positives[i] / float(true_positives[i]+false_positives[i])
+            recall = true_positives[i] / float(true_positives[i]+false_negatives[i])
+            fscore = 2 * (precision * recall) / float(precision + recall)
+        print(i, fscore)
+    print("\n")
--- place_dashes.py
+++ place_dashes.py
@@ -5,7 +5,7 @@ import sys
 
 def main(argv):
     text_data = []
-    output = open("en.tok.off.pos.test", "w")
+    output = open(argv[1]+"2", "w")
     with open(argv[1], 'r') as fname:
         for line in fname:
             text_data.append(line.split())
@@ -14,8 +14,9 @@ def main(argv):
             i.extend(("-", "-"))
         elif len(i) == 6:
             i.append("-")
-        print(i[3])
+        print[i]
         data = ("{:4}{:4}{:6}{:20}{:5}{:5}{}".format(i[0], i[1], i[2], i[3], i[4], i[5], i[6]))
         output.write(data+"\n")
+        output.close()
 
 main(sys.argv)
