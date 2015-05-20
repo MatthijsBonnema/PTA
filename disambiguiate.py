594b91c4f7292317be5602fa01edabad2bdc416c
MatthijsBonnema <m.s.bonnema@student.rug.nl>
2015-05-20 21:49:40 +0200

    Push dat shit

--- disambiguiate.py
+++ disambiguiate.py
@@ -9,8 +9,11 @@ import sys
 def main(argv):
     ambigious_words = []
     ambigious_lines = []
+    text_words = []
     with open(argv[1], 'r') as fname:
         for line in fname:
+            split_line = line.split()
+            text_words.append(split_line)
             l = line.split()
             if l[4] == "NN" or l[4] == "NNP":
                 if len(wordnet.synsets(l[3], "n")) > 1:
@@ -18,17 +21,13 @@ def main(argv):
     for word in ambigious_words:
         start = int(str(word[0][0]) + "001")
         end = start + 999
-        with open(argv[1], 'r') as fname:
-            lines = []
-            for line in fname:
-                l = line.split()
-                if start <= int(l[2]) <= end:
-                    lines.append(l[3])
-                    print(l[3])
-            ambigious_lines.append(lines)
+        lines = []
+        for l in text_words:
+            if start <= int(l[2]) <= end:
+                lines.append(l[3])
+        ambigious_lines.append(lines)
     for i in range(len(ambigious_lines)):
         print(ambigious_words[i], ambigious_lines[i])
-        print(i)
 
 
 
