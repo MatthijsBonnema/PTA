dc1fde695e9b05fbb6e0ce5b08a112b8732f3784
simondewit <simon@witoos.nl>
2015-05-26 13:11:24 +0200

    disambiguiate

---
+++ disambiguiate.py
@@ -0,0 +1,13 @@
+__author__ = 'simon'
+
+from nltk.corpus import wordnet
+from nltk.wsd import lesk
+from nltk import word_tokenize
+import sys
+
+
+def main(argv):
+    with open(argv[1], 'r') as fname:
+
+
+main(sys.argv)
