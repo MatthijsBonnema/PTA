e52b0854b0de980c89bcb50c1265c137c3540156
simondewit <simon@witoos.nl>
2015-05-26 13:22:07 +0200

    disambiguiate

--- disambiguiate.py
+++ disambiguiate.py
@@ -7,10 +7,16 @@ import sys
 
 
 def main(argv):
+    ambigious_words = []
     with open(argv[1], 'r') as fname:
         for line in fname:
             l = line.split()
-            if l[5]
+            if l[5] == "NN" or l[5] == "NNP":
+                if len(wordnet.synsets(l[5], "n")) > 1:
+                    ambigious_words.append((l[2], l[3]))
+
+
+
 
 
 main(sys.argv)
