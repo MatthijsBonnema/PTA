27b601212e6c6c7364754d2603260521a8e22675
simondewit <simon@witoos.nl>
2015-05-26 14:25:26 +0200

    disambiguiate

--- disambiguiate.py
+++ disambiguiate.py
@@ -1,4 +1,4 @@
-__author__ = 'simon'
+#!/usr/bin/python
 
 from nltk.corpus import wordnet
 from nltk.wsd import lesk
@@ -15,24 +15,20 @@ def main(argv):
             if l[4] == "NN" or l[4] == "NNP":
                 if len(wordnet.synsets(l[3], "n")) > 1:
                     ambigious_words.append((l[2], l[3]))
-    with open(argv[1], 'r') as fname:
-        for word in ambigious_words:
-            x = 0
-            start = int(str(word[x][0]) + "001")
-            print(start)
-            end = start + 999
+    for word in ambigious_words:
+        start = int(str(word[0][0]) + "001")
+        end = start + 999
+        with open(argv[1], 'r') as fname:
             for line in fname:
                 l = line.split()
                 if str(l[2]).endswith("001"):
                     lines = []
-                print(lines)
                 if start <= int(l[2]) <= end:
                     lines.append(l[3])
                 ambigious_lines.append(lines)
-            x += 1
-    # print(ambigious_words, ambigious_lines)
+    print(ambigious_lines, ambigious_words)
     # for i in range(len(ambigious_lines)):
-        # print(ambigious_words[i], ambigious_lines[i])
+    #     print(ambigious_words[i], ambigious_lines[i])
 
 
 
