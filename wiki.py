588a471948e695c9aa0dc8bc288ba40609f3bc67
MatthijsBonnema <m.s.bonnema@student.rug.nl>
2015-05-28 09:30:26 +0200

    Push dat shit

---
+++ wiki.py
@@ -0,0 +1,15 @@
+# File: wikipedia.py
+# Author: Matthijs Bonnema
+# Date: 5/28/15
+# Info: 
+
+import sys
+import wikipedia
+
+
+def main():
+    print(wikipedia.search("Barack"))
+
+
+if __name__ == "__main__":
+    main()
