419ca5e35dc55a1f1c30ea9a5f103327a50415d2
simondewit <simon@witoos.nl>
2015-06-04 09:43:17 +0200

    postagging final assignment

--- wiki.py
+++ wiki.py
@@ -7,98 +7,82 @@ from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import wordnet
 import nltk
 from collections import defaultdict
+from nltk.tag.stanford import NERTagger
 
-def main():
+
+def main(argv):
     print(wikipedia.search("chair"))
 
 
-def nertagger(list):
-    # print(file)
-    relative = wordnet.synsets("relative", pos='n')
-    science = wordnet.synsets("science", pos='n')
-    illness = wordnet.synsets("illness", pos='n')
+def posTagger(text_data):
+    # Take the 4th column, the word
+    tokens = [token_data[3] for token_data in text_data]
+    tagged_tokens = nltk.pos_tag(tokens)
+    for i in range(len(text_data)):
+        text_data[i].append(tagged_tokens[i][1])
+    output = open("en.tok.off.pos", "w")
+    for i in text_data:
+        data = ("{:4}{:4}{:6}{:20}{:3}".format(i[0], i[1], i[2], i[3], i[4]))
+        output.write(data+"\n")
+    output.close()
 
-    entities = {
-        "act": wordnet.synsets("act", pos='n'),
-        "animal": wordnet.synsets("animal", pos='n'),
-        "artifact": wordnet.synsets("artifact", pos='n'),
-        "attribute": wordnet.synsets("attribute", pos='n'),
-        "body": wordnet.synsets("body", pos='n'),
-        "cognition": wordnet.synsets("cognition", pos='n'),
-        "communication": wordnet.synsets("communication", pos='n'),
-        "event": wordnet.synsets("event", pos='n'),
-        "feeling": wordnet.synsets("feeling", pos='n'),
-        "food": wordnet.synsets("food", pos='n'),
-        "group": wordnet.synsets("group", pos='n'),
-        "location": wordnet.synsets("location", pos='n'),
-        "motive": wordnet.synsets("motive", pos='n'),
-        "natural_object": wordnet.synsets("natural object", pos='n'),
-        "natural_phenomenon": wordnet.synsets("natural object", pos='n'),
-        "person": wordnet.synsets("person", pos='n'),
-        "plant": wordnet.synsets("plant", pos='n'),
-        "possession": wordnet.synsets("possession", pos='n'),
-        "process": wordnet.synsets("process", pos='n'),
-        "quantity": wordnet.synsets("quantity", pos='n'),
-        "relation": wordnet.synsets("relation", pos='n'),
-        "shape": wordnet.synsets("shape", pos='n'),
-        "state": wordnet.synsets("state", pos='n'),
-        "substance": wordnet.synsets("substance", pos='n'),
-        "time": wordnet.synsets("time", pos='n')
-    }
 
-    noun_lemmas = []
-    noun_synsets_relative = []
-    noun_synsets_illness = []
-    noun_synsets_science = []
-    lemmatizer = WordNetLemmatizer()
-    pos_tags = nltk.pos_tag(list)
-
-    for word in pos_tags:
+def nertagger(fname):
+    with open(fname, "r") as inp_file:
         if word[1] == "NN" or word[1] == "NNP":
-            # print(word)
-            noun_lemmas.append(lemmatizer.lemmatize(word[0], wordnet.NOUN))
-            # get the synsets for the noun 'honey'
-            word_synset = wordnet.synsets(word[0], pos="n")
-            if len(word_synset) != 0 and len(relative) != 0 and len(science) != 0 and len(illness) != 0:
-                if hypernymOf(word_synset[0], relative[0]):
-                    noun_synsets_relative.append(word_synset)
-                if hypernymOf(word_synset[0], illness[0]):
-                    noun_synsets_illness.append(word_synset)
-                if hypernymOf(word_synset[0], science[0]):
-                    noun_synsets_science.append(word_synset)
+        words = ["Barack Obama", "Holland", "Government", "Tennis", "happiness"]
 
-    # Exercise 1.1
-    print("EXERCISE 1.1\n")
-    print("Relative: ", len(noun_synsets_relative))
-    print("Illness: ", len(noun_synsets_illness))
-    print("Science: ", len(noun_synsets_science))
+        noun_lemmas = []
+        nouns = []
+        final_ner_tagged = []
+        not_ner_tagged = []
+        pos_tags = nltk.pos_tag(words)
+        lemmatizer = WordNetLemmatizer()
 
-    # Exercise 1.2
-    print("\nEXERCISE 1.2\n")
-    tagged_top_entities = defaultdict(list)
-    for word in pos_tags:
-        if word[1] == "NN" or word[1] == "NNP":
-            noun_lemmas.append(lemmatizer.lemmatize(word[0], wordnet.NOUN))
-            word_synset = wordnet.synsets(word[0], pos="n")
-            for e in list(entities.keys()):
-                if len(word_synset) != 0 and len(entities[e]) != 0:
-                    if hypernymOf(word_synset[0], entities[e][0]):
-                        tagged_top_entities[word[0]].append(e)
-    for w in tagged_top_entities:
-        print("{:15}{:15}".format(w, tagged_top_entities[w]))
+        class3 = NERTagger('stanford-ner/classifiers/english.all.3class.distsim.crf.ser.gz',
+                           'stanford-ner/stanford-ner.jar')
 
-    # Exercise 1.3
-    print("\nEXERCISE 1.3\n")
-    compare_words = [("car", "automobile"), ("coast", "shore"), ("food", "fruit"), ("journey", "car"),
-                     ("monk", "slave"), ("moon", "string")]
-    similarity_words = []
-    for words in compare_words:
-        w1synsets = wordnet.synsets(words[0], pos="n")
-        w2synsets = wordnet.synsets(words[1], pos="n")
-        similarity_words.append((words[0], words[1], getMaxSim(w1synsets, w2synsets)))
-    similarity_words.sort(key=lambda tup: tup[2], reverse=True)
-    for item in similarity_words:
-        print("{:15}{:5}{:15}{:15}".format(item[0], "<>", item[1], item[2]))
+        # STANFORD NERTAGGING HAPPENS HERE
+        for tag in pos_tags:
+            if tag[1] == 'NNP':
+                nouns.append(tag[0])
+            elif tag[1] == 'NN':
+                nouns.append(tag[0])
+
+        ner_tagged = class3.tag(nouns)
+        for t in ner_tagged[0]:
+            if t[1] == u'O':
+                not_ner_tagged.append(t[0])
+            else:
+                final_ner_tagged.append(t)
+        print("NERTagged:")
+        print(final_ner_tagged)
+
+        entities = {
+            "COUNTRY": wordnet.synsets("country", pos='n'),
+            "STATE": wordnet.synsets("state", pos='n'),
+            "CITY": wordnet.synsets("city", pos='n'),
+            "TOWN": wordnet.synsets("town", pos='n'),
+            "NAT": wordnet.synsets("natural places", pos='n'),
+            "PER": wordnet.synsets("person", pos='n'),
+            "ORG": wordnet.synsets("organisation", pos='n'),
+            "ANI": wordnet.synsets("animal", pos='n'),
+            "SPO": wordnet.synsets("sport", pos='n'),
+            "ENT": wordnet.synsets("entertainment", pos='n'),
+        }
+
+        tagged_top_entities = defaultdict(list)
+        for word in pos_tags:
+            if word[1] == "NN" or word[1] == "NNP":
+                noun_lemmas.append(lemmatizer.lemmatize(word[0], wordnet.NOUN))
+                word_synset = wordnet.synsets(word[0], pos="n")
+                for e in list(entities.keys()):
+                    if len(word_synset) != 0 and len(entities[e]) != 0:
+                        if hypernymOf(word_synset[0], entities[e][0]):
+                            tagged_top_entities[word[0]].append(e)
+        print("WordNet tagged:")
+        for w in tagged_top_entities:
+            print("{:15}{:15}".format(w, tagged_top_entities[w]))
 
 
 def hypernymOf(synset1, synset2):
