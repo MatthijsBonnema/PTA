f6c6ee3338eae7d9a3aaadcfcd664c37cf086cf5
simondewit <simon@witoos.nl>
2015-06-04 10:43:16 +0200

    postagging + nertagging final assignment

--- wiki.py
+++ wiki.py
@@ -3,7 +3,6 @@
 
 import sys
 import wikipedia
-from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import wordnet
 import nltk
 from collections import defaultdict
@@ -13,6 +12,13 @@ from nltk.tag.stanford import NERTagger
 def main(argv):
     print(wikipedia.search("chair"))
 
+    text = []
+    with open(argv[1], 'r') as filedata:
+        for line in filedata:
+            text.append(line.split())
+    posTagger(text)
+    entityTagger()
+
 
 def posTagger(text_data):
     # Take the 4th column, the word
@@ -20,37 +26,38 @@ def posTagger(text_data):
     tagged_tokens = nltk.pos_tag(tokens)
     for i in range(len(text_data)):
         text_data[i].append(tagged_tokens[i][1])
-    output = open("en.tok.off.pos", "w")
+    output = open("en.tok.off.test.pos", "w")
     for i in text_data:
         data = ("{:4}{:4}{:6}{:20}{:3}".format(i[0], i[1], i[2], i[3], i[4]))
         output.write(data+"\n")
     output.close()
 
 
-def nertagger(fname):
+def entityTagger():
     class3 = NERTagger('stanford-ner/classifiers/english.all.3class.distsim.crf.ser.gz',
                        'stanford-ner/stanford-ner.jar')
-    output = open("en.tok.off.pos.tagged", "w")
-    with open(fname, "r") as inp_file:
+    output = open("en.tok.off.test.pos.tagged", "w")
+    with open("en.tok.off.test.pos", "r") as inp_file:
         for l in inp_file:
             line = l.split()
+            print(line[3])
             if line[4] == "NN" or line[4] == "NNP":
-                ner_tagged = class3.tag(line[3])
-                print(ner_tagged)
-                for t in ner_tagged:
-                    if t[1] == u'O':
+                ner_tagged = class3.tag([line[3]])
+                print("Nertagged:", ner_tagged)
+                for t in ner_tagged[0]:
+                    if len(t[1]) < 3:
                         #WordNet tagging
                         tag = wordNetTagger(t[0])
-                        data = ("{:4}{:4}{:6}{:20}{:3}{:10}".format(line[0], line[1], line[2], line[3], line[4], tag))
+                        print("Wordnet tag:", tag)
+                        data = ("{:4}{:4}{:6}{:20}{:6}{:10}".format(line[0], line[1], line[2], line[3], line[4], tag))
                         output.write(data+"\n")
                     else:
-                        data = ("{:4}{:4}{:6}{:20}{:3}{:10}".format(line[0], line[1], line[2], line[3], line[4], t[1]))
+                        data = ("{:4}{:4}{:6}{:20}{:6}{:10}".format(line[0], line[1], line[2], line[3], line[4], t[1]))
                         output.write(data+"\n")
     output.close()
 
 
 def wordNetTagger(w):
-    lemmatizer = WordNetLemmatizer()
     entities = {
         "COUNTRY": wordnet.synsets("country", pos='n'),
         "STATE": wordnet.synsets("state", pos='n'),
@@ -63,9 +70,6 @@ def wordNetTagger(w):
         "SPO": wordnet.synsets("sport", pos='n'),
         "ENT": wordnet.synsets("entertainment", pos='n'),
     }
-
-    tagged_top_entities = defaultdict(list)
-    lemmatizer.lemmatize(w, wordnet.NOUN)
     word_synset = wordnet.synsets(w, pos="n")
     for e in list(entities.keys()):
         if len(word_synset) != 0 and len(entities[e]) != 0:
@@ -90,4 +94,4 @@ def hypernymOf(synset1, synset2):
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
